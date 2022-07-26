7ff8e8b4ebbba8bd74784534ac224c8f5240a433
gokul <gokulkrishna1282@gmail.com>
2022-07-27 03:48:38 +0530

    My Portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:my_port/portfolios/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDS2jRKKJdnOLSthsZpotL0V_JIoKz1Bto",
+      authDomain: "ct---pro.firebaseapp.com",
+      projectId: "ct---pro",
+      storageBucket: "ct---pro.appspot.com",
+      messagingSenderId: "1009818783243",
+      appId: "1:1009818783243:web:e9856bc8e66c50da87410e",
+      measurementId: "G-VERKFMXHX8",
+    ),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Portfolio',
+      title: 'Gokul Krishna',
       home:  MyHomePage(),
     );
   }
